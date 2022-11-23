608e721dcb610e4584b00bbb77203696c23a51db
DhavanKanakia <dkanaki1@stevens.edu>
2022-11-23 18:00:41 -0500

    Declared data functions and mongo setup

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -1,1 +1,22 @@
 //mongoCollection details
+const dbConnection = require('./mongoConnection');
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {
+  users: getCollectionFn('users'),
+  restaurants: getCollectionFn('restaurants'),
+  reviews: getCollectionFn('reviews'),
+  reservations: getCollectionFn('reservations')
+};
--- data/restaurants.js
+++ data/restaurants.js
@@ -1,1 +1,21 @@
 // restaurants data functions
+const mongoCollections = require("../config/mongoCollections");
+const restaurants = mongoCollections.restaurants;
+
+async function createRestaurant(){}
+
+async function getAllRestaurants(){}
+
+async function getRestaurantById(){}
+
+async function removeRestaurant(){}
+
+async function updateRestaurant(){}
+
+module.exports = {
+    createRestaurant,
+    getAllRestaurants,
+    getRestaurantById,
+    removeRestaurant,
+    updateRestaurant
+}
