2e55301398c4e2fcbc0ec033e5567aeb8d3176fd
Manzoor Mehaboob <manzoormehaboob@MEHABOMA-2.local>
2022-12-09 22:16:37 -0500

    initial route setup

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-// index routes
+const restaurantRoutes = require('./restaurants');
+
+const constructorMethod = (app) => {
+  app.use('', restaurantRoutes);
+
+
+  app.use('*', (req, res) => {
+    res.redirect('/');
+  });
+};
+
+module.exports = constructorMethod;
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,1 +1,10 @@
-// restaurants routes
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const eestaurantData = data.restaurants;
+
+router.get("/", async (req, res) => {
+  res.render("restaurants/home", { users: "users" });
+});
+
+module.exports = router;
