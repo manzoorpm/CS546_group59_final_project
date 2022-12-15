7a1bbae9d3684448e6e4d564b8995d28bdc753bc
Mao Li <mli94@stevens.edu>
2022-12-15 00:26:30 -0500

    added review and delete review functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,8 @@
 // front end javascript module
+
+
+$(".deleteReview").click(function () {
+    window.location.href = "/restaurant/delete-review/" + $(this).data('id');
+});
+
+$('.alert').alert();
